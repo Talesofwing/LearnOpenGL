1f78e8cbc995bf6b00a5ea61f2a9f678f1d5570f
zer0 <zerox0103@gmail.com>
2026-06-13 10:24:55 +0900

    Revert exercise

--- LearnOpenGL/src/shaders/shader.fs
+++ LearnOpenGL/src/shaders/shader.fs
@@ -5,12 +5,10 @@ out vec4 FragColor;
 in vec3 ourColor;
 in vec2 TexCoord;
 
-uniform float interp;
-
 uniform sampler2D texture1;
 uniform sampler2D texture2;
 
 void main()
 {
-	FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), interp);
+	FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
 };
