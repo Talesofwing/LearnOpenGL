9f61ed531240dae7d31647beb41b8259fe72de28
zer0 <zerox0103@gmail.com>
2026-06-15 15:52:34 +0900

    Revert exercise

--- LearnOpenGL/src/main.cpp
+++ LearnOpenGL/src/main.cpp
@@ -22,37 +22,6 @@ bool firstMouse = true;
 // Camera
 Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
 
-// Custom implementation of the LookAt function
-glm::mat4 calculate_lookAt_matrix(glm::vec3 position, glm::vec3 target, glm::vec3 worldUp) {
-	// 1. Position = known
-	// 2. Calculate cameraDirection
-	glm::vec3 zaxis = glm::normalize(position - target);
-	// 3. Get positive right axis vector
-	glm::vec3 xaxis = glm::normalize(glm::cross(glm::normalize(worldUp), zaxis));
-	// 4. Calculate camera up vector
-	glm::vec3 yaxis = glm::cross(zaxis, xaxis);
-
-	// Create translation and rotation matrix
-	// In glm we access elements as mat[col][row] due to column-major layout
-	glm::mat4 translation = glm::mat4(1.0f); // Identity matrix by default
-	translation[3][0] = -position.x; // Third column, first row
-	translation[3][1] = -position.y;
-	translation[3][2] = -position.z;
-	glm::mat4 rotation = glm::mat4(1.0f);
-	rotation[0][0] = xaxis.x; // First column, first row
-	rotation[1][0] = xaxis.y;
-	rotation[2][0] = xaxis.z;
-	rotation[0][1] = yaxis.x; // First column, second row
-	rotation[1][1] = yaxis.y;
-	rotation[2][1] = yaxis.z;
-	rotation[0][2] = zaxis.x; // First column, third row
-	rotation[1][2] = zaxis.y;
-	rotation[2][2] = zaxis.z;
-
-	// Return lookAt matrix as combination of translation and rotation matrix
-	return rotation * translation; // Remember to read from right to left (first translation then rotation)
-}
-
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 	glViewport(0, 0, width, height);
 }
@@ -267,11 +236,7 @@ int main() {
 		
 		glBindVertexArray(VAO);
 
-		const float radius = 10.0f;
-		float camX = sin(glfwGetTime()) * radius;
-		float camZ = cos(glfwGetTime()) * radius;
-		//glm::mat4 view = camera.GetViewMatrix();
-		glm::mat4 view = calculate_lookAt_matrix(glm::vec3(camX, 0.0f, camZ), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+		glm::mat4 view = camera.GetViewMatrix();
 		ourShader.setMat4("view", view);
 
 		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / SCR_HEIGHT, 0.1f, 100.0f);
