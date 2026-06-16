536a75f79851ba78d94eebf3be47e747f91f8488
zer0 <zerox0103@gmail.com>
2026-06-16 17:53:49 +0900

    Basic Lighting: Exercises3

--- LearnOpenGL/src/shaders/shader.fs
+++ LearnOpenGL/src/shaders/shader.fs
@@ -1,33 +1,10 @@
 #version 330 core
 
-in vec3 WorldPos;
-in vec3 Normal;
-
-uniform vec3 objectColor;
-uniform vec3 lightColor;
-
-uniform vec3 lightPos;
-uniform vec3 viewPos;
+in vec3 Color;
 
 out vec4 FragColor;
 
 void main()
 {
-    vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(lightPos - WorldPos);
-    vec3 viewDir = normalize(viewPos - WorldPos);
-    vec3 reflectDir = reflect(-lightDir, norm);
-
-    float ambientStrength = 0.1;
-    vec3 ambient = ambientStrength * lightColor;
-
-    float diffuseFactor = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = diffuseFactor * lightColor;
-
-    float specularStrength = 0.9;
-    float specularFactor = pow(max(dot(viewDir, reflectDir), 0.0), 128);
-    vec3 specular = specularStrength * specularFactor * lightColor;
-
-    vec3 result = (ambient + diffuse + specular) * objectColor;
-    FragColor = vec4(result, 1.0);
+    FragColor = vec4(Color, 1.0);
 }
